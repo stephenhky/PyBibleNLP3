45f9a17ae84703a3f8539e83a8334d5ff61ae1f3
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-06-11 17:42:53 -0400

    console modularized

--- biblechapter_retriever.py
+++ biblechapter_retriever.py
@@ -16,6 +16,7 @@ def get_argparser():
     argparser.add_argument('sbert_model', help='Name of the Sentence BERT model from huggingface.co (see sbert.net), or the path of the Sentence BERT model')
     argparser.add_argument('featurestore_h5', help='H5 file for feature-store.')
     argparser.add_argument('--nbrecords', type=int, default=5, help='Number of record to show. (Default: 5)')
+    argparser.add_argument('--threshold', type=float, default=0.0, help='Threshold to truncate. (Default: 0.0)')
     return argparser
 
 
@@ -36,13 +37,13 @@ def retrieve_bible_chapters(text, sbert_transformer, featurestore_retriever, thr
     return allmatches
 
 
-if __name__ == '__main__':
-    args = get_argparser().parse_args()
-    bible_db = args.bible_db
-    sbert_model = args.sbert_model
-    featurestore_h5_path = args.featurestore_h5
-    nbrecords = args.nbrecords
-
+def bible_retriever_console(
+        bible_db,
+        sbert_model,
+        featurestore_h5_path,
+        nbrecords=5,
+        threshold=0.0
+):
     connector = BibleSQLiteConnector(bible_db)
     sbert_transformer = SentenceBERTSentenceFeatureVectorGenerator(sbert_model)
     featurestore_retriever = BibleH5FeatureStoreRetriever(featurestore_h5_path)
@@ -53,7 +54,7 @@ if __name__ == '__main__':
         if text is None or len(text) <= 0:
             done = True
         else:
-            sorted_chapters = retrieve_bible_chapters(text, sbert_transformer, featurestore_retriever)
+            sorted_chapters = retrieve_bible_chapters(text, sbert_transformer, featurestore_retriever, threshold=threshold)
             for i, chapter in enumerate(sorted_chapters):
                 if i > nbrecords:
                     break
@@ -63,3 +64,20 @@ if __name__ == '__main__':
                 print('Similarity: {:.6f}'.format(chapter['similarity']))
 
     featurestore_retriever.close()
+
+
+if __name__ == '__main__':
+    args = get_argparser().parse_args()
+    bible_db = args.bible_db
+    sbert_model = args.sbert_model
+    featurestore_h5_path = args.featurestore_h5
+    nbrecords = args.nbrecords
+    threshold = args.threshold
+
+    bible_retriever_console(
+        bible_db,
+        sbert_model,
+        featurestore_h5_path,
+        nbrecords=nbrecords,
+        threshold=threshold
+    )
